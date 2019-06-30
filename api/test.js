9961c549f2c2a3f56eff0442a39082878b8e340f
b2blosangeles <b2blosangeles@gmail.com>
2019-06-30 06:53:58 -0700

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,21 +1,4 @@
-/*
-const { Client } = require(env.site_path + '/api/inc/pg/node_modules/pg');
-const client = new Client({
-  host: 'my.database-server.com',
-  port: 5334,
-  user: 'database-user',
-  password: 'secretpassword!!',
-})
-client.connect((err) => {
-  if (err) {
-    res.send(err.message);
-    // console.error('connection error', err.stack)
-  } else {
-    res.send('connected');
-    // console.log('connected')
-  }
-})
-*/
+
 var pg = require(env.site_path + '/api/inc/pg/node_modules/pg');
 var setting =  require(env.config_path + '/dbSetting.json');
 
@@ -43,3 +26,22 @@ client.connect(function(err) {
     client.end();
   });
 });
+
+/*
+const { Client } = require(env.site_path + '/api/inc/pg/node_modules/pg');
+const client = new Client({
+  host: 'my.database-server.com',
+  port: 5334,
+  user: 'database-user',
+  password: 'secretpassword!!',
+})
+client.connect((err) => {
+  if (err) {
+    res.send(err.message);
+    // console.error('connection error', err.stack)
+  } else {
+    res.send('connected');
+    // console.log('connected')
+  }
+})
+*/
