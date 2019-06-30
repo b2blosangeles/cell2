3b20b7aaa1bb6d244ca4a628b9f53d3f55d1080b
b2blosangeles <b2blosangeles@gmail.com>
2019-06-29 22:57:19 -0700

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,10 +1,12 @@
 const { Client } = require(env.site_path + '/api/inc/pg/node_modules/pg');
-const client = new Client()
 
+const client = new Client({
+  connectionString: connectionString,
+})
 client.connect()
 
-client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
-  console.log(err ? err.stack : res.rows[0].message) // Hello World!
+client.query('SELECT NOW()', (err, res) => {
+  // console.log(err, res)
   res.send('env');
   client.end()
 });
