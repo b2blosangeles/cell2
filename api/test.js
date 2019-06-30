c6da46e498871b7c1d18e1f6bc2e76fc66f238fa
b2blosangeles <b2blosangeles@gmail.com>
2019-06-29 22:54:25 -0700

    Update test.js

--- api/test.js
+++ api/test.js
@@ -5,6 +5,6 @@ client.connect()
 
 client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
   console.log(err ? err.stack : res.rows[0].message) // Hello World!
-  res.send(env);
+  res.send('env');
   client.end()
 });
