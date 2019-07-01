e7ae29bad9dfaa4601ec6cb64b10d33d96b35830
b2blosangeles <b2blosangeles@gmail.com>
2019-06-30 23:33:05 -0700

    Update testE.js

--- api/testE.js
+++ api/testE.js
@@ -1,6 +1,6 @@
 var elasticsearch = require(env.site_path + '/api/inc/elasticsearch/node_modules/elasticsearch');
 var client = new elasticsearch.Client({
-  host: 'search-ebitietest-2fe3gbehhusko6gpdceqbzxnaq.us-west-1.es.amazonaws.com',
+  host: 'search-ebiztieprod-ywa24bvy6tjusrpec3dqc3elry.us-west-1.es.amazonaws.com',
   log: 'trace'
 });
 // 'pgdb.dev.shusiou.win:9200'
@@ -10,7 +10,7 @@ client.ping({
   requestTimeout: 1000
 }, function (error) {
   if (error) {
-    res.send('elasticsearch cluster is down 2!');
+    res.send('elasticsearch cluster is down 32!');
   } else {
     res.send('All is well');
   }
