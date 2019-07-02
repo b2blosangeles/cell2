c12f95135c291c0e6294314e17c0572b7c6a2bf6
b2blosangeles <b2blosangeles@gmail.com>
2019-07-01 21:13:55 -0700

    Update testE.js

--- api/testE.js
+++ api/testE.js
@@ -1,24 +1,13 @@
 var elasticsearch = require(env.root_path + '/vendor/elasticsearch/node_modules/elasticsearch');
 var setting =  require(env.config_path + '/dbSetting.json');
 
+res.send(setting.dev.elasticsearch);
+return true;
+
 var client = new elasticsearch.Client({
   host: setting.dev.elasticsearch,
   log: 'trace'
 });
-// 'pgdb.dev.shusiou.win:9200'
-// https://search-ebitietest-2fe3gbehhusko6gpdceqbzxnaq.us-west-1.es.amazonaws.com/
-/*
-client.ping({
-  // ping usually has a 3000ms timeout
-  requestTimeout: 1000
-}, function (error) {
-  if (error) {
-    res.send('elasticsearch cluster is down 32!');
-  } else {
-    res.send('All is well');
-  }
-});
-*/
 
 try {
   client.search({
@@ -33,12 +22,3 @@ try {
 } catch (error) {
   res.send(error.message)
 }
-/*
-client.search({
-  index: 'my-index',
-  body: { foo: 'bar' }
-}, (err, result) => {
-  if (err) res.send(error.message);
-  else res.send(result);
-})
-*/
