27478cd587456fa71ffc8537cc61128dd95ff027
b2blosangeles <b2blosangeles@gmail.com>
2019-07-02 05:42:46 -0700

    Update testE.js

--- api/testE.js
+++ api/testE.js
@@ -1,9 +1,6 @@
 var elasticsearch = TAO.require(TAO.env.root_path + '/vendor/elasticsearch/node_modules/elasticsearch');
 var setting =  TAO.require(TAO.env.config_path + '/dbSetting.json');
 
-TAO.res.send(setting.dev.elasticsearch);
-return true;
-
 var client = new elasticsearch.Client({
   host: setting.dev.elasticsearch,
   log: 'trace'
