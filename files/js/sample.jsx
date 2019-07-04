8d056de8b996858af8b082b749cb84778a191d76
b2blosangeles <b2blosangeles@gmail.com>
2019-07-04 12:54:28 -0700

    Update sample.jsx

--- files/js/sample.jsx
+++ files/js/sample.jsx
@@ -3,27 +3,7 @@ class HelloMessage extends React.Component {
 	super(props);
 	this.state = {};
 	var me = this;
-	$.ajax({
-	     type: 'POST',
-	     url: '/api/testRestful.api',
-	     data: {},
-	     dataType: 'JSON',
-	     timeout: (6 * 1000),
-	     success: function(resultData){
-		     console.log('---resultData--->');
-		     console.log(resultData.data.val);
-		     me.val = resultData.data.val;
-		     $(document).ready(function() {
-			  ReactDOM.render(
-			    <HelloMessage name="" />,
-			    document.getElementById('bob')
-			  );
-			});
-	     },
-	     error : function(xhr, textStatus, error) { 
-	     }
-	  }); 
-	// this.val = 1234;
+
   }
   render() {
     return (
@@ -34,5 +14,25 @@ class HelloMessage extends React.Component {
     );
   }
 }
+$(document).ready(function() {
+	$.ajax({
+	     type: 'POST',
+	     url: '/api/testRestful.api',
+	     data: {},
+	     dataType: 'JSON',
+	     timeout: (6 * 1000),
+	     success: function(resultData){
+		  var val = resultData.data.val;
+		  ReactDOM.render(
+		    <HelloMessage name={val} />,
+		    document.getElementById('bob')
+		  );
+			
 
+	     },
+	     error : function(xhr, textStatus, error) { 
+	     }
+	  }); 
+
+});
 
