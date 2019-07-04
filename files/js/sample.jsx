a58b949fa786ba16a277a734c15b06d412fc5d7e
b2blosangeles <b2blosangeles@gmail.com>
2019-07-04 12:44:02 -0700

    Update sample.jsx

--- files/js/sample.jsx
+++ files/js/sample.jsx
@@ -2,6 +2,19 @@ class HelloMessage extends React.Component {
   constructor(props) {
 	super(props);
 	this.state = {};
+	$.ajax({
+	     type: 'POST',
+	     url: '/api/testRestful.api',
+	     data: {},
+	     dataType: 'JSON',
+	     timeout: (cfg.timeout) ? cfg.timeout : (6 * 1000),
+	     success: function(resultData){
+		     console.log('---resultData--->');
+		     console.log(resultData);
+	     },
+	     error : function(xhr, textStatus, error) { 
+	     }
+	  }); 
 	this.val = 1234;
   }
   render() {
