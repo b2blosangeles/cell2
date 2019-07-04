2263dd8fb14a3aadb3b76911a5c3b271681099d2
b2blosangeles <b2blosangeles@gmail.com>
2019-07-04 11:10:52 -0700

    Update sample.jsx

--- files/js/sample.jsx
+++ files/js/sample.jsx
@@ -3,10 +3,22 @@ class HelloMessage extends React.Component {
     return (
       <div>
         Hello {this.props.name}
+        <hr/>
+        <PelloMessage/>
       </div>
     );
   }
 }
+class PelloMessage extends React.Component {
+  render() {
+    return (
+      <div>
+        PelloMessage
+      </div>
+    );
+  }
+}
+
 $(document).ready(function() {
   ReactDOM.render(
     <HelloMessage name="BOB" />,
