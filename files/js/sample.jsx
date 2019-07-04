ffb57361d8ede0405a6e0473c0f4d88f294d3c2a
b2blosangeles <b2blosangeles@gmail.com>
2019-07-04 10:04:23 -0700

    Update sample.jsx

--- files/js/sample.jsx
+++ files/js/sample.jsx
@@ -12,7 +12,7 @@ $(document).ready(function() {
     <HelloMessage name="BOB" />,
     document.getElementById('bob')
   );
-/*---3---*/
+/*---4---*/
   ReactDOM.render(
     <HelloMessage name="DOCr" />,
     document.getElementById('doc')
