6e1e3659684bb7fbc39f952623ef027efc5cad36
b2blosangeles <b2blosangeles@gmail.com>
2019-07-04 23:47:52 -0700

    Update TaoController.js

--- files/js/TaoController.js
+++ files/js/TaoController.js
@@ -2,3 +2,6 @@ ReactDOM.TAOController = new Object();
 ReactDOM.TAOController.append = function(obj, pobj) {
     ReactDOM.render(obj, pobj.appendChild( document.createElement( 'div' )));
 }
+ReactDOM.TAOController.load = function(obj, pobj) {
+    ReactDOM.render(obj, pobj);
+}
