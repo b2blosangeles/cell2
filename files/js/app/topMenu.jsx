8db721966a8d45725743a8d37f43ce9ce7b779e6
b2blosangeles <b2blosangeles@gmail.com>
2019-07-05 11:25:27 -0700

    Create topMenu.jsx

---
+++ files/js/app/topMenu.jsx
@@ -0,0 +1,39 @@
+class topMenu extends React.Component {
+  constructor(props) {
+	super(props);
+	this.props = props;
+	ReactDOM.TAO.register(this);
+	this.state = {};
+  }
+  componentDidMount() {
+	var me = this;
+	ReactDOM.TAO.ajax({
+	     type: 'POST',
+	     url: '/api/testRestful.api',
+	     data: {},
+	     dataType: 'JSON',
+	     timeout: (6 * 1000),
+	     success: function(resultData){
+		  me.setState(resultData.data);
+	     },
+	     error : function(err) { 
+	     }
+	  });   
+  }
+  componentDidUpdate(prevProps, prevState) {
+	var me = this;
+	console.log('---prevStat-->');
+	console.log(prevState);
+	  console.log('---me.State-=->');
+	console.log(me.state);
+  }
+  render() {
+    return (
+      <span>
+        Hello {this.state.hello} -XX- {this.state.bb} 
+        <hr/> {this.state.v} <hr/>
+	{(this.state._TAOstate) ? this.state._TAOstate.bb : ''}
+      </span>
+    );
+  }
+}
