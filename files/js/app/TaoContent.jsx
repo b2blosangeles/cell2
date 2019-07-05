74759cde88cfcd8d6a985e7ca7b07f34f8b8c2e0
b2blosangeles <b2blosangeles@gmail.com>
2019-07-05 16:02:39 -0700

    Create TaoContent.jsx

---
+++ files/js/app/TaoContent.jsx
@@ -0,0 +1,22 @@
+class TopContent extends React.Component {
+  constructor(props) {
+	super(props);
+	this.props = props;
+	ReactDOM.TAO.register(this);
+	this.state = {};
+  }
+  componentDidMount() {
+	var me = this;
+  }
+  componentDidUpdate(prevProps, prevState) {
+	var me = this;
+  }
+  render() {
+    return (
+      <div className="border border-success alert-success rounded  m-0 mt-3 mb-2 p-2">
+        <h1>Tao Development</h1>        
+	{(this.state._TAOstate) ? this.state._TAOstate.hello : ''}
+      </div>
+    );
+  }
+}
