0df49230acf00299cbc2b35b89ecae4cb1468ed3
b2blosangeles <b2blosangeles@gmail.com>
2019-07-05 09:24:04 -0700

    Update sample.jsx

--- files/js/sample.jsx
+++ files/js/sample.jsx
@@ -48,7 +48,7 @@ class COMApp extends React.Component {
 	     timeout: (6 * 1000),
 	     success: function(resultData){
 		me.setState(resultData.data);
-		ReactDOM.TAO.setState('Tao', {bb : 'CCCCMMM'});
+		ReactDOM.TAO.setState('*', {bb : 'CCCCMMM'});
 	     },
 	     error : function(xhr, textStatus, error) { 
 	     }
