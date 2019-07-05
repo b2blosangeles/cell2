704849a362d368033380ccaf6946e546f1c75d27
b2blosangeles <b2blosangeles@gmail.com>
2019-07-05 16:05:24 -0700

    Update TAOindex.jsx

--- files/js/app/TAOindex.jsx
+++ files/js/app/TAOindex.jsx
@@ -2,5 +2,6 @@ $(document).ready(function() {
 	ReactDOM.TAO.append('Root', <TAORoot param={{}} />, document.body);
 	ReactDOM.TAO.load('topMenu', <TopMenu param={{}} />, document.getElementById('topMenu'));
 	ReactDOM.TAO.load('leftBox', <LeftBox param={{}} />, document.getElementById('leftBox'));
+	ReactDOM.TAO.load('rightContent', <RightContent param={{}} />, document.getElementById('rightContent'));
 });
 
