f2c5420a875d9dec06a86cb694b582ad6c5f65f4
b2blosangeles <b2blosangeles@gmail.com>
2019-07-04 18:16:45 -0700

    Update sample.jsx

--- files/js/sample.jsx
+++ files/js/sample.jsx
@@ -27,9 +27,44 @@ class TAOApp extends React.Component {
     );
   }
 }
+class COMApp extends React.Component {
+  constructor(props) {
+	super(props);
+	this.state = {val : '111'};
+  }
+  componentDidMount() {
+	var me = this;
+	$.ajax({
+	     type: 'POST',
+	     url: '/api/testRestful.api',
+	     data: {},
+	     dataType: 'JSON',
+	     timeout: (6 * 1000),
+	     success: function(resultData){
+		  me.setState(resultData.data);
+	     },
+	     error : function(xhr, textStatus, error) { 
+	     }
+	  });   
+  }
+  render() {
+    return (
+      <span>
+        Hello {this.state.hello}
+        <hr/> {this.state.v} <hr/>
+      </span>
+    );
+  }
+}
 $(document).ready(function() {
 	ReactDOM.render(
 		<TAOApp param={{}} />,
+		document.body.appendChild( document.createElement( 'div' ) )
+		// document.getElementById('bob')
+	);
+	
+	ReactDOM.render(
+		<COMApp param={{}} />,
 		document.body.appendChild( document.createElement( 'div' ) )
 		// document.getElementById('bob')
 	);
