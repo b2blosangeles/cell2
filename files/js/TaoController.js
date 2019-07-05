fe4178302a84067f7841a98c1abfb5fcaf11a475
b2blosangeles <b2blosangeles@gmail.com>
2019-07-05 09:54:59 -0700

    Update TaoController.js

--- files/js/TaoController.js
+++ files/js/TaoController.js
@@ -1,30 +1,32 @@
-ReactDOM.TAO = new Object();
-ReactDOM.TAO.list = {};
-ReactDOM.TAO.append = function(id, obj, pobj) {
-    obj.props._TID = id;
-    ReactDOM.render(obj, pobj.appendChild( document.createElement( 'div' )));
-}
-ReactDOM.TAO.load = function(id, obj, pobj) {
-    obj.props._TID = id;
-    ReactDOM.render(obj, pobj);
-}
-
-ReactDOM.TAO.register = function(obj) {
-    ReactDOM.TAO.list[obj.props._TID] = obj;
-}
-
-ReactDOM.TAO.setState = function(id, data) {
-    if (id === '*') {
-        for (o in ReactDOM.TAO.list) {
-            ReactDOM.TAO.list[o].setState({_TTM: new Date().getTime(), _TDATA: data});
+(function() {
+    var TAO = {
+        list    : {},
+        append  : function(id, obj, pobj) {
+            obj.props._TID = id;
+            ReactDOM.render(obj, pobj.appendChild( document.createElement( 'div' )))
+        },
+        load : function(id, obj, pobj) {
+            obj.props._TID = id;
+            ReactDOM.render(obj, pobj);
+        },
+        register : function(obj) {
+            ReactDOM.TAO.list[obj.props._TID] = obj;
+        },
+        setState : function(id, data) {
+            if (id === '*') {
+                for (o in ReactDOM.TAO.list) {
+                    ReactDOM.TAO.list[o].setState({_TTM: new Date().getTime(), _TDATA: data});
+                }
+            } else if (Array.isArray(id)) {
+                for (var i = 0; i < id.length; i++) {
+                     if (ReactDOM.TAO.list[id[i]]) {
+                         ReactDOM.TAO.list[id[i]].setState({_TTM: new Date().getTime(), _TDATA: data});
+                     }
+                }
+            } else if (ReactDOM.TAO.list[id]) {
+                 ReactDOM.TAO.list[id].setState({_TTM: new Date().getTime(), _TDATA: data});
+            }
         }
-    } else if (Array.isArray(id)) {
-        for (var i = 0; i < id.length; i++) {
-             if (ReactDOM.TAO.list[id[i]]) {
-                 ReactDOM.TAO.list[id[i]].setState({_TTM: new Date().getTime(), _TDATA: data});
-             }
-        }
-    } else if (ReactDOM.TAO.list[id]) {
-         ReactDOM.TAO.list[id].setState({_TTM: new Date().getTime(), _TDATA: data});
     }
-}
+    ReactDOM.TAO = TAO;
+})() 
