68775fb455c9cc6534724ec2138823d3ee083fab
b2blosangeles <b2blosangeles@gmail.com>
2019-07-05 15:10:54 -0700

    Update LeftBox.jsx

--- files/js/app/LeftBox.jsx
+++ files/js/app/LeftBox.jsx
@@ -3,7 +3,7 @@ class LeftBox extends React.Component {
 	super(props);
 	this.props = props;
 	ReactDOM.TAO.register(this);
-	this.state = {};
+	this.state = {list : []};
   }
   componentDidMount() {
 	var me = this;  
