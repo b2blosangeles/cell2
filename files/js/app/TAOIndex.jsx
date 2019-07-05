9498d7dc0577b5b0fc1ec24d546f61206989ff1c
b2blosangeles <b2blosangeles@gmail.com>
2019-07-05 14:09:38 -0700

    Update TAOIndex.jsx

--- files/js/app/TAOIndex.jsx
+++ files/js/app/TAOIndex.jsx
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	ReactDOM.TAO.append('Root', <TAORoot param={{}} />, document.body);
 	ReactDOM.TAO.load('topMenu', <TopMenu param={{}} />, document.getElementById('topMenu'));
+	ReactDOM.TAO.load('leftMenu', <TopMenu param={{}} />, document.getElementById('leftMenu'));
 });
 
