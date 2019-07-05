a7ad0fbd5a3150e501ec31ca6b94218473a0298c
b2blosangeles <b2blosangeles@gmail.com>
2019-07-05 14:17:58 -0700

    Create LeftBox.jsx

---
+++ files/js/app/LeftBox.jsx
@@ -0,0 +1,23 @@
+class LeftBox extends React.Component {
+  constructor(props) {
+	super(props);
+	this.props = props;
+	ReactDOM.TAO.register(this);
+	this.state = {};
+  }
+  componentDidMount() {
+	var me = this;
+  }
+  componentDidUpdate(prevProps, prevState) {
+	var me = this;
+  }
+  render() {
+    return (
+      <span>
+        TopMenu
+	<hr/>	        
+	{(this.state._TAOstate) ? this.state._TAOstate.hello : ''}
+      </span>
+    );
+  }
+}
