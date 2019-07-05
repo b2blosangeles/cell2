5f6adf71f0d0ce6b88087daa6d3ae32fc90b8749
b2blosangeles <b2blosangeles@gmail.com>
2019-07-05 09:25:12 -0700

    Update TaoController.js

--- files/js/TaoController.js
+++ files/js/TaoController.js
@@ -13,7 +13,7 @@ ReactDOM.TAO.setState = function(id, data) {
         for (o in ReactDOM.TAO.list) {
             ReactDOM.TAO.list[o].setState(data);
         }
-    } else (if (ReactDOM.TAO.list[id]) ) {
+    } else if (ReactDOM.TAO.list[id]) ) {
          ReactDOM.TAO.list[id].setState(data);
     }
 }
