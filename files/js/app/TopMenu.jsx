a6b8fbb43dd8e7df6c8177beec4c3145eec4bd0a
b2blosangeles <b2blosangeles@gmail.com>
2019-07-05 11:55:52 -0700

    Update and rename topMenu.jsx to TopMenu.jsx

---
+++ files/js/app/TopMenu.jsx
@@ -0,0 +1,21 @@
+class TopMenu extends React.Component {
+  constructor(props) {
+	super(props);
+	this.props = props;
+	ReactDOM.TAO.register(this);
+	this.state = {};
+  }
+  componentDidMount() {
+	var me = this;
+  }
+  componentDidUpdate(prevProps, prevState) {
+	var me = this;
+  }
+  render() {
+    return (
+      <span>
+        TopMenu 
+      </span>
+    );
+  }
+}
