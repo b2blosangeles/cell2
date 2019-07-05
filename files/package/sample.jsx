2a218c0267884e90b2b04b18ce01a1388ec395c7
b2blosangeles <b2blosangeles@gmail.com>
2019-07-05 10:50:39 -0700

    Update sample.jsx

--- files/package/sample.jsx
+++ files/package/sample.jsx
@@ -1,15 +1,15 @@
 {
 	"filelist":[
-      	"/js/json2/json2.min.js",
-	"/js/jquery/3.3.1/jquery.min.js",
-	"/js/jqueryui/1.12.1/jquery-ui.min.js",
-	"/js/react/react.min.js",
-	"/js/react/react-dom.min.js",
-	"/js/react/react-cookie.0.4.9.min.js",
-	"/js/react/ReactRouter/3.0.2/ReactRouter.min.js",
-		
-	"/js/app/TaoController.jsx",
-	"/js/app/TAORoot.jsx",
-	"/js/app/sample.jsx"
+		"/js/json2/json2.min.js",
+		"/js/jquery/3.3.1/jquery.min.js",
+		"/js/jqueryui/1.12.1/jquery-ui.min.js",
+		"/js/react/react.min.js",
+		"/js/react/react-dom.min.js",
+		"/js/react/react-cookie.0.4.9.min.js",
+		"/js/react/ReactRouter/3.0.2/ReactRouter.min.js",
+
+		"/js/app/TaoController.jsx",
+		"/js/app/TAORoot.jsx",
+		"/js/app/sample.jsx"
 	]
 }
