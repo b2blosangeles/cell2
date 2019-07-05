054259d1b99dc32ab5c256cf300532888b96e254
b2blosangeles <b2blosangeles@gmail.com>
2019-07-04 23:54:52 -0700

    Update sample.jsx

--- files/js/sample.jsx
+++ files/js/sample.jsx
@@ -2,7 +2,7 @@ class TAOApp extends React.Component {
   constructor(props) {
 	super(props);
 	this.props = props;
-	ReactDOM.TAOController[this.constructor.name] = this;
+	ReactDOM.TAOController.list[this.constructor.name] = this;
 	this.state = {val : '111', bb : 'AAA'};
   }
   componentDidMount() {
@@ -33,7 +33,7 @@ class TAOApp extends React.Component {
 class COMApp extends React.Component {
   constructor(props) {
 	super(props);
-	ReactDOM.TAOController[this.constructor.name] = this;
+	ReactDOM.TAOController.list[this.constructor.name] = this;
 	this.state = {val : '111', bb : 'BBB'};
   }
   componentDidMount() {
@@ -46,7 +46,7 @@ class COMApp extends React.Component {
 	     timeout: (6 * 1000),
 	     success: function(resultData){
 		  me.setState(resultData.data);
-		  ReactDOM.TAOController.TAOApp.setState({bb : 'CCCC'});
+		  ReactDOM.TAOController.list.TAOApp.setState({bb : 'CCCC'});
 		//  TAOApp.alert();   
 	     },
 	     error : function(xhr, textStatus, error) { 
