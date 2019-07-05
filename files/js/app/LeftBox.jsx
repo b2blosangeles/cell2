0716f0c639eff7e2585fd1cf78f00161882f1b7d
b2blosangeles <b2blosangeles@gmail.com>
2019-07-05 15:06:25 -0700

    Update LeftBox.jsx

--- files/js/app/LeftBox.jsx
+++ files/js/app/LeftBox.jsx
@@ -6,7 +6,20 @@ class LeftBox extends React.Component {
 	this.state = {};
   }
   componentDidMount() {
-	var me = this;
+	var me = this;  
+	ReactDOM.TAO.ajax({
+	     type: 'POST',
+	     url: '/api/Tao/API_PG.api',
+	     data: {},
+	     dataType: 'JSON',
+	     timeout: (6 * 1000),
+	     success: function(resultData){
+		  me.setState({list : resultData.data, _TM : new Date().getTime()});
+	     },
+	     error : function(err) { 
+	     }
+	  });   
+	  
   }
   componentDidUpdate(prevProps, prevState) {
 	var me = this;
