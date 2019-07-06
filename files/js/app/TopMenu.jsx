8b1d3f7d4a8244eb5ae6368e71910a8baab3cfe8
b2blosangeles <b2blosangeles@gmail.com>
2019-07-05 17:48:48 -0700

    Update TopMenu.jsx

--- files/js/app/TopMenu.jsx
+++ files/js/app/TopMenu.jsx
@@ -32,7 +32,7 @@ class TopMenu extends React.Component {
 	<hr/>
 	<p className="wrapper">
 		{this.state.list.map(function(item, i){
-             		return item.tablename + ' '
+             		return <a>item.tablename</a>
            	})}
 	</p>
       </div>
