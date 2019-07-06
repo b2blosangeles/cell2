57a04833851a8341e66a9b0f03956f1acacce540
b2blosangeles <b2blosangeles@gmail.com>
2019-07-05 17:13:54 -0700

    Update TopMenu.jsx

--- files/js/app/TopMenu.jsx
+++ files/js/app/TopMenu.jsx
@@ -29,7 +29,7 @@ class TopMenu extends React.Component {
     return (
       <div className="border border-success alert-success rounded  m-0 mt-3 mb-2 p-2 d-flex">
         <h1>Tao Development</h1>
-	<br/>
+	<hr/>
 	<div className="p-1">
 		{this.state.list.map(function(item, i){
              		return (<div className="float-left block mr-3">{item.tablename}</div>)
