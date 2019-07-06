c152b1f5e5b13e593d6b663bd111f33096d83235
b2blosangeles <b2blosangeles@gmail.com>
2019-07-05 19:31:35 -0700

    Update TopMenu.jsx

--- files/js/app/TopMenu.jsx
+++ files/js/app/TopMenu.jsx
@@ -7,6 +7,21 @@ class TopMenu extends React.Component {
   }
   componentDidMount() {
 	var me = this; 
+	  
+	ReactDOM.TAO.list.Root.loadData({
+	     type: 'POST',
+	     url: '/api/Tao/API_PG.api?code=PGdatabases',
+	     data: {},
+	     dataType: 'JSON',
+	     timeout: (6 * 1000),
+	     success: function(resultData){
+		  me.setState({list : resultData.data, _TM : new Date().getTime()});
+	     },
+	     error : function(err) { 
+		     console.log('err');
+	     }
+	});
+	  
 	ReactDOM.TAO.ajax({
 	     type: 'POST',
 	     url: '/api/Tao/API_PG.api?code=PGdatabases',
