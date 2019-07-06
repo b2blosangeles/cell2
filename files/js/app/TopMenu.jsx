1a86c69fff563f2a8547d72f94c51b6106bebba8
b2blosangeles <b2blosangeles@gmail.com>
2019-07-05 17:01:35 -0700

    Update TopMenu.jsx

--- files/js/app/TopMenu.jsx
+++ files/js/app/TopMenu.jsx
@@ -31,7 +31,7 @@ class TopMenu extends React.Component {
         <h1>Tao Development</h1>
 	<div className="d-inline p-1">
 		{this.state.list.map(function(item, i){
-             		return (<div className="float-none mr-3">{item.tablename}</div>)
+             		return (<div className="float-left d-inline mr-3">{item.tablename}</div>)
            	})}
 		<div className="float-none clear">kkk</div>
 	</div>
