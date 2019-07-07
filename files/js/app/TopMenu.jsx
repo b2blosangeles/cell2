f5177df1614be5046fe81c20fe1538e24237bc06
b2blosangeles <b2blosangeles@gmail.com>
2019-07-06 23:53:34 -0700

    Update TopMenu.jsx

--- files/js/app/TopMenu.jsx
+++ files/js/app/TopMenu.jsx
@@ -16,7 +16,7 @@ class TopMenu extends React.Component {
 	     timeout: (6 * 1000),
 	     success: function(resultData){
 		  me.setState({list : resultData.data, _TM : new Date().getTime()});
-		  ReactDOM.TAO.list.Root.popup({data : 7789, closeIcon: true});
+		  ReactDOM.TAO.popup({data : 'ABCD', closeIcon: true});
 	     },
 	     error : function(err) { 
 		     console.log('err');
