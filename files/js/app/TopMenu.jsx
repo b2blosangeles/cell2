8a2493fa3880cccdac0eaf92e9450afe73d10be8
b2blosangeles <b2blosangeles@gmail.com>
2019-07-07 00:11:54 -0700

    Update TopMenu.jsx

--- files/js/app/TopMenu.jsx
+++ files/js/app/TopMenu.jsx
@@ -15,7 +15,7 @@ class TopMenu extends React.Component {
 	     dataType: 'JSON',
 	     timeout: (6 * 1000),
 	     success: function(resultData){
-		  me.setState({list : resultData.data});
+		  me.setState({list : resultData.data, _TM : new Date().getTime()});
 	     },
 	     error : function(err) { 
 		     console.log('err');
