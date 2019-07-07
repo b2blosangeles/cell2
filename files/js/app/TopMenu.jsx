b0438f169ed2402269f133971832190535a02d90
b2blosangeles <b2blosangeles@gmail.com>
2019-07-06 21:14:40 -0700

    Update TopMenu.jsx

--- files/js/app/TopMenu.jsx
+++ files/js/app/TopMenu.jsx
@@ -8,7 +8,7 @@ class TopMenu extends React.Component {
   componentDidMount() {
 	var me = this;
 	  console.log(ReactDOM.findDOMNode(me));
-	ReactDOM.findDOMNode(me).append('ttt');
+	ReactDOM.findDOMNode(me).append(ReactDOM.TAO.list.Root.showBoxSpinner());
 	ReactDOM.TAO.list.Root.loadData({
 	     type: 'POST',
 	     url: '/api/Tao/API_PG.api?code=PGdatabases',
