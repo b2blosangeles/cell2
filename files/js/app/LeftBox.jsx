16aef4bd8c64f5803d95c3a28653af83ae480488
b2blosangeles <b2blosangeles@gmail.com>
2019-07-07 06:51:09 -0700

    Update LeftBox.jsx

--- files/js/app/LeftBox.jsx
+++ files/js/app/LeftBox.jsx
@@ -29,7 +29,7 @@ class LeftBox extends React.Component {
 	  var me = this;
     return (
       <div className="border border-warning alert-warning rounded m-0 p-2">
-	{me.loadSpinner(me)}
+	{me._spinner(me)}
 	ALL TABLES :
         <ul>
           {this.state.list.map(function(item, i){
