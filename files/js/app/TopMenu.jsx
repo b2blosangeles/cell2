2b79016f2413e0bc5bacf3cf152ea082b39977e6
b2blosangeles <b2blosangeles@gmail.com>
2019-07-07 18:57:27 -0700

    Update TopMenu.jsx

--- files/js/app/TopMenu.jsx
+++ files/js/app/TopMenu.jsx
@@ -32,7 +32,7 @@ class TopMenu extends React.Component {
   }
   render() {
 	var me = this;
-	return (<span>{ReactDOM.TAO.list.Root.showSpinner(me)}
+	return (
 	<div className="border border-success alert-success rounded  m-0 mt-3 mb-2 p-2">
 		{/*ReactDOM.TAO.list.Root.showSpinner(me)*/}
 		<h1>Tao Development</h1>
@@ -46,7 +46,7 @@ class TopMenu extends React.Component {
 			})}
 		</p>
 
-	</div></span>
+	</div>
 	);
   }
 }
