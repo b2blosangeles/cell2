7be810d2f5a4db96150b4ffa0fc97f203269f5b6
b2blosangeles <b2blosangeles@gmail.com>
2019-07-07 20:16:43 -0700

    Update TaoController.jsx

--- files/js/app/TaoController.jsx
+++ files/js/app/TaoController.jsx
@@ -12,7 +12,7 @@
 			var newObjB = pobj.appendChild(document.createElement( 'div' ));
 			
 			this.list[id] = ReactDOM.render(obj, newObjB);
-			ReactDOM.render(ReactDOM.TAO.list.Root.showSpinner(obj), newObjA);
+			ReactDOM.render(<span>kkk---{ReactDOM.TAO.list.Root.showSpinner(this.list[id])}</span>, newObjA);
 			/*
 			if (ReactDOM.TAO.list.Root) {
 				this.list[id]._spinner = function(o) {
