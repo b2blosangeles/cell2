6766e2b1230a151c08ee494bf2957e0b4c055079
b2blosangeles <b2blosangeles@gmail.com>
2019-07-07 19:49:32 -0700

    Update TopMenu.jsx

--- files/js/app/TopMenu.jsx
+++ files/js/app/TopMenu.jsx
@@ -28,15 +28,11 @@ class TopMenu extends React.Component {
   handleClick(item) {
 	ReactDOM.TAO.popup({data : item.datname, closeIcon: true});
   }
-  _spinner(o) {
-	  return 'AAAA'
-  }
   render() {
 	var me = this;
 	return (
 	<div className="border border-success alert-success rounded  m-0 mt-3 mb-2 p-2">
-			{me. _spinner(me)}
-		{/* ReactDOM.TAO.list.Root.showSpinner(me) */}
+		{ReactDOM.TAO.list.Root.showSpinner(me)}
 		<h1>Tao Development</h1>
 		<p className="p-2">
 			{this.state.list.map(function(item, i){
