f921e5cf71abb1178875e08ec0bee32548f01ee8
b2blosangeles <b2blosangeles@gmail.com>
2019-07-22 06:30:48 -0700

    addIoCommunication

---
+++ files/package/ioCommunication.js
@@ -0,0 +1,9 @@
+{
+	"filelist" : [
+		"/js/json2/json2.min.js",
+		"/js/jquery/3.3.1/jquery.min.js",
+		"/js/jqueryui/1.12.1/jquery-ui.min.js",
+		"/js/crowdProcess/crowdProcess.js",
+		"/js/socket.io/1.7.3/socket.io.min.js"
+	]
+}
