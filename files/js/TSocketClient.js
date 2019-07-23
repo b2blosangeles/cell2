0cdf3e276ecdb9a7957423caec91da42d99d00f5
b2blosangeles <b2blosangeles@gmail.com>
2019-07-23 13:09:16 -0700

    Update TSocketClient.js

--- files/js/TSocketClient.js
+++ files/js/TSocketClient.js
@@ -1,14 +1,23 @@
 (function () { 
   var obj =  function () {
     
-    this.socket = null;
-    
-    this.connection = function(url) {
-      this.socket = io(url);
-    }
-    this.getRoomClients = function() {
-    //  alert('TSocketClient');
-    }
+      this.socket = null;
+
+      this.connection = function(url) {
+        this.socket = io(url);
+      }
+      this.getRoomClients = function() {
+        var cp = new crowdProcess(), _f = {};
+       _f('A') = function(cbk) {
+          cbk(true);
+       }      
+       cp.serial(
+          _f,
+         function() {
+           alert('TSocketClient2');
+         }, 6000
+       )
+      }
   }
   if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
       module.exports = obj;
