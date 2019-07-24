70a38aa9f8f3dad4422e6f7f86f1676476860b34
b2blosangeles <b2blosangeles@gmail.com>
2019-07-24 14:13:07 -0700

    Update TSocketClient.js

--- files/js/TSocketClient.js
+++ files/js/TSocketClient.js
@@ -56,7 +56,7 @@
       }
       this.disConnect = function() {
         var me = this;
-        me.socket.disconnect();
+        me.socket.emit('disconnect');
       }
   }
   if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
