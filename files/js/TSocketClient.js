b5804112c9858ef6a285b654a9fdd12f9e9b9a70
b2blosangeles <b2blosangeles@gmail.com>
2019-07-26 22:58:10 -0700

    Update TSocketClient.js

--- files/js/TSocketClient.js
+++ files/js/TSocketClient.js
@@ -6,6 +6,8 @@
       this._sessions = {}
       this.events = { 
           roomCilents : function(data, session_id){
+            console.log('--session_id--->')
+            console.log(session_id);
               if (!data || !data.session_id) return true;
               var s = data.session_id.split('.')
               _ROOT._sessions[s[1]] = function(cbk) {
