4d4460f0a03c727f127213e6094d88be8e680bea
b2blosangeles <b2blosangeles@gmail.com>
2019-07-27 22:18:24 -0700

    Update TSocketClient.js

--- files/js/TSocketClient.js
+++ files/js/TSocketClient.js
@@ -65,7 +65,7 @@
               });
       };
     
-      this.sendToClient = function ( toClientId, data, clientInfo) { 
+      this.sendToClient = function ( toClientId, data) { 
          me = this;
          _ROOT._SN = (!_ROOT._SN || _ROOT._SN > 9999) ? 1 : (_ROOT._SN + 1);
          var session_id = '' + _ROOT._SN;
@@ -74,7 +74,6 @@
                 toClient    : toClientId,
                 session_id  : session_id,
                 data        : {
-                        clientInfo  : encodeURIComponent(JSON.stringify(clientInfo)),
                         data  : encodeURIComponent(JSON.stringify(data))
                 }
               });
