04264e9825971c9eab99a4c011069c5ff4734c84
b2blosangeles <b2blosangeles@gmail.com>
2019-08-02 23:18:47 -0700

    Update TSocketClient.js

--- files/js/TSocketClient.js
+++ files/js/TSocketClient.js
@@ -150,7 +150,7 @@
       }
       this.getRoomHosts = function (v, func) {
           var me = this;
-       // return true;
+        return true;
           me.getRoomClients(v, function(data) {
              var list = (!data.data.clients) ? {} : data.data.clients;
              var hosts = {};
