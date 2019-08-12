a28d14a862c579f9b4910a8e132b0a831b5fc75f
b2blosangeles <b2blosangeles@gmail.com>
2019-08-11 19:23:03 -0700

    Update TSocketClient.js

--- files/js/TSocketClient.js
+++ files/js/TSocketClient.js
@@ -20,7 +20,7 @@
                 if ((income_data) && (income_data.code) && (me.trigger[income_data.code]) && (typeof me.trigger[income_data.code] === 'function')) {
                     me.trigger[income_data.code](income_data);
                 } else {
-                    console.log('_incomeNotice_ coming---->>111>>');
+                    console.log('_incomeMessage_ coming---->>111>>');
                     console.log(income_data)      
                 }
             });          
