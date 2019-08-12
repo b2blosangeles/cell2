3c04200cddd60cbb1e4f173bfd5cb72dc8267157
b2blosangeles <b2blosangeles@gmail.com>
2019-08-11 23:21:32 -0700

    Update ioCommunication.js

--- files/package/ioCommunication.js
+++ files/package/ioCommunication.js
@@ -4,6 +4,7 @@
 		"/js/jquery/3.3.1/jquery.min.js",
 		"/js/jqueryui/1.12.1/jquery-ui.min.js",
 		"/js/crowdProcess/crowdProcess.js",
-		"/js/socket.io/1.7.3/socket.io.min.js"
+		"/js/socket.io/1.7.3/socket.io.min.js",
+		"js/TSocketComm.js"
 	]
 }
