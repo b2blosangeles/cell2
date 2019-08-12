23288d15da08a03d76ea39a6db8a43d9f35929e5
b2blosangeles <b2blosangeles@gmail.com>
2019-08-11 23:18:02 -0700

    Update TSocketClient.js

--- files/js/TSocketClient.js
+++ files/js/TSocketClient.js
@@ -22,7 +22,7 @@
                 let list = data.list;
                 var svr =  list[Math.floor(Math.random() * list.length)];
                 var link = ((data.isSSL) ? 'https://' : 'http://') + svr + '/';
-                me._room[room + '_' +  svr] = new TSocketComm(url);
+                me._room[room + '_' +  svr] = new window.TSocketComm(url);
                 func(me._room);
             });
         } 
