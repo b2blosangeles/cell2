9e45cc72dd8f2a63a8bb2dca87f24f6bf77db606
b2blosangeles <b2blosangeles@gmail.com>
2019-08-11 19:46:04 -0700

    Update TSocketClient.js

--- files/js/TSocketClient.js
+++ files/js/TSocketClient.js
@@ -1,41 +1,4 @@
 (function () { 
-    var obj =  function () {
-
-        this.connection = function(cbk) {
-            var me = this;
-            me.socket = io(url);
-
-            me.socket.on('_incomeMessage_', function(income_data) {
-                if ((income_data) && (income_data.code) && (me.trigger[income_data.code]) && (typeof me.trigger[income_data.code] === 'function')) {
-                    me.trigger[income_data.code](income_data);
-                } else {
-                    console.log('_incomeMessage_ coming---->>111>>');
-                    console.log(income_data)      
-                }
-            });          
-            
-            me.socket.on('connect', function() {
-                console.log('connected-->');
-                if (typeof cbk === 'function') { 
-                    console.log('cbk---');
-                    
-                    cbk();
-                } 
-            });  
-            me.socket.on('disconnect', function(reason) {
-                console.log(reason);
-            }); 
-
-        }
-    }   
-    if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-        module.exports = obj;
-    } else {
-        window.TSocketClientCOMM = obj; 
-    }
-})();
-
-(function () { 
     var obj =  function (url) {
         this.getCommServers = function (func) {
             var me = this;
