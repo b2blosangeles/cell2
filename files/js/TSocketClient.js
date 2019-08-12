18c9f1c89eb6cddddebb191648ae5290d37515ed
b2blosangeles <b2blosangeles@gmail.com>
2019-08-11 19:40:05 -0700

    Update TSocketClient.js

--- files/js/TSocketClient.js
+++ files/js/TSocketClient.js
@@ -1,9 +1,6 @@
 (function () { 
-    var obj =  function (url) {
-        this.getCommServers = function (func) {
-            var me = this;
-            me.socket.emit('clientRequest', {cmd: 'roomServers'},  func);
-        }
+    var obj =  function () {
+
         this.connection = function(cbk) {
             var me = this;
             me.socket = io(url);
@@ -35,6 +32,42 @@
             });  
             me.socket.on('disconnect', function(reason) {
                 console.log(reason);
+            }); 
+
+        }
+    }   
+    if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+        module.exports = obj;
+    } else {
+        window.TSocketClientCOMM = obj; 
+    }
+})();
+
+(function () { 
+    var obj =  function (url) {
+        this.getCommServers = function (func) {
+            var me = this;
+            me.socket.emit('clientRequest', {cmd: 'roomServers'},  func);
+        }
+        this.connection = function(cbk) {
+            var me = this;
+            me.socket = io(url);
+            me.socket.on('_incomeMessage_', function(income_data) {
+                if ((income_data) && (income_data.code) && (me.trigger[income_data.code]) && (typeof me.trigger[income_data.code] === 'function')) {
+                    me.trigger[income_data.code](income_data);
+                } else {
+                    console.log('_incomeMessage_ coming---->>111>>');
+                    console.log(income_data)      
+                }
+            });          
+            
+            me.socket.on('connect', function() {
+                if (typeof cbk === 'function') { 
+                    cbk();
+                } 
+            });  
+            me.socket.on('disconnect', function(reason) {
+                console.log('IO disconnected :: ' + reason);
             }); 
 
         }
