69339786da3c9ed1512f1ff54dd997c5eb8cc213
b2blosangeles <b2blosangeles@gmail.com>
2019-08-11 23:29:15 -0700

    Update TSocketComm.js

--- files/js/TSocketComm.js
+++ files/js/TSocketComm.js
@@ -1,19 +1,10 @@
 (function () { 
     var obj =  function (url) {
 
-        this.connection = function(cbk) {
+        this.init = function(cbk) {
             var me = this;
             me.socket = io(url);
 
-            me.socket.on('_incomeMessage_', function(income_data) {
-                if ((income_data) && (income_data.code) && (me.trigger[income_data.code]) && (typeof me.trigger[income_data.code] === 'function')) {
-                    me.trigger[income_data.code](income_data);
-                } else {
-                    console.log('_incomeMessage_ coming---->>111>>');
-                    console.log(income_data)      
-                }
-            });          
-            
             me.socket.on('connect', function() {
                 console.log('connected-->');
                 if (typeof cbk === 'function') { 
@@ -27,6 +18,9 @@
             }); 
 
         }
+        this.init(function() {
+            console.log('init --> ' + url);
+        })
     }
     window.TSocketCOMM = obj;
 })();
