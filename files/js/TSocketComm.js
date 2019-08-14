74af54753361e7a079acdc7f79adf17094dde76c
b2blosangeles <b2blosangeles@gmail.com>
2019-08-14 08:28:21 -0700

    Update TSocketComm.js

--- files/js/TSocketComm.js
+++ files/js/TSocketComm.js
@@ -31,6 +31,7 @@
             me = this;
             me.socket.emit('clientRequest', {
                 cmd         : 'joinRoom',
+                room : room,
                 data        : {room : room }
             }, func);
         };
