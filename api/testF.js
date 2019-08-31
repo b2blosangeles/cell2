c9aa0f9589b15d75d89d2422bc0990962d328a16
b2blosangeles <b2blosangeles@gmail.com>
2019-08-31 07:11:25 -0700

    Update testF.js

--- api/testF.js
+++ api/testF.js
@@ -3,15 +3,29 @@ TAO.pkg.fs.readFile(fn, 'utf8', function read(err, data) {
     if (err) {
       TAO.res.send(false);
     } else {
-      var DL =  [], DR = {};
+      var DL =  [], DS = {};
       try { DL = data.split("\n"); } catch(e) {}
       for (var i = 0; i < DL.length; i++) {
             DL[i] = DL[i].split('=>');
             if (DL[i].length == 2) {
-                DR[decodeURIComponent(DL[i][0].replace(/^\s+|\s+$/gm,''))] = decodeURIComponent(DL[i][1].replace(/^\s+|\s+$/gm,''))
+                DS[decodeURIComponent(DL[i][0].replace(/^\s+|\s+$/gm,''))] = decodeURIComponent(DL[i][1].replace(/^\s+|\s+$/gm,''))
             }
       } 
-      TAO.res.send(DR);
+        var name = 'tao_jxu_dev.shusiou.win';
+    for (var key in DS) {
+        var re = new RegExp(key, 'ig');
+        if (re.test(name)) {
+            TAO.res.send([{ 
+                name: name,
+                type: 'A',
+                class: 'IN',
+                ttl: 600,
+                data: DS[key]
+            }], req, res);
+        }
     }
+  
+      TAO.res.send('DR');
+ 
     
 });
