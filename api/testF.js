5a433e06876e3d0a7253ade32cd3566d5f4ad36d
b2blosangeles <b2blosangeles@gmail.com>
2019-08-31 06:24:30 -0700

    Update testF.js

--- api/testF.js
+++ api/testF.js
@@ -1,11 +1,11 @@
-let fn = env.config_path + '/dynamic_dns.json';
-pkg.fs.readFile(fn, 'utf8', function read(err, data) {
+let fn = TAO.env.config_path + '/dynamic_dns.json';
+TAO.pkg.fs.readFile(fn, 'utf8', function read(err, data) {
     if (err) {
-      res.send(false);
+      TAO.res.send(false);
     } else {
       var DS = {};
       try { DS = JSON.parse(data); } catch(e) {}
 
-      res.send(DS);
+      TAO.res.send(DS);
     }
 });
