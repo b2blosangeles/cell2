63a0cb031f485c7136bd0689391d294d0428fa59
b2blosangeles <b2blosangeles@gmail.com>
2019-08-31 06:36:30 -0700

    Update testF.js

--- api/testF.js
+++ api/testF.js
@@ -1,4 +1,4 @@
-let fn = TAO.env.config_path + '/rule_dns.json';
+let fn = TAO.env.config_path + '/rule_dns.txt';
 TAO.pkg.fs.readFile(fn, 'utf8', function read(err, data) {
     if (err) {
       TAO.res.send(false);
