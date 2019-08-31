15172cf9259fd051614ae52dff3c59da68f58d1d
b2blosangeles <b2blosangeles@gmail.com>
2019-08-31 15:34:03 -0700

    Update testF.js

--- api/testF.js
+++ api/testF.js
@@ -1,31 +1,33 @@
+let name = 'taop_jxu_dev.shusiou.win';
+
 let fn = TAO.env.config_path + '/rule_dns.data';
 TAO.pkg.fs.readFile(fn, 'utf8', function read(err, data) {
     if (err) {
       TAO.res.send(false);
     } else {
       var DL =  [], DS = {};
+        
       try { DL = data.split("\n"); } catch(e) {}
       for (var i = 0; i < DL.length; i++) {
             DL[i] = DL[i].split('=>');
             if (DL[i].length == 2) {
-                DS[DL[i][0].replace(/^\s+|\s+$/gm,'')] = DL[i][1].replace(/^\s+|\s+$/gm,'') 
+                let key = DL[i][0].replace(/^\s+|\s+$/gm,''),
+                    ip =  DL[i][1].replace(/^\s+|\s+$/gm,'');
+                
+                var re = new RegExp(key, 'ig');
+                if (re.test(name)) {
+                    TAO.res.send([{ 
+                        name: name,
+                        type: 'A',
+                        class: 'IN',
+                        ttl: 600,
+                        data: ip
+                    }]);
+                    return true
+                }
             }
       } 
-        var name = 'tao_jxu_dev.shusiou.win';
-        for (var key in DS) {
-            var re = new RegExp(key, 'ig');
-            if (re.test(name)) {
-                TAO.res.send([{ 
-                    name: name,
-                    type: 'A',
-                    class: 'IN',
-                    ttl: 600,
-                    data: DS[key]
-                }]);
-            }
-        }
-  
-      TAO.res.send('DR');
+      TAO.res.send('DR--');
     }
     
 });
