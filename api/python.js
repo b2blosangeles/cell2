f9da1d7446b3e81be7e12c05200dfc2cfbf6e893
b2blosangeles <b2blosangeles@gmail.com>
2019-10-12 21:06:01 -0700

    Update python.js

--- api/python.js
+++ api/python.js
@@ -3,7 +3,7 @@ var exec = TAO.require('child_process').exec;
 var CP = new TAO.pkg.crowdProcess();
 var _f = {};
 switch((TAO.req.body.code) ? TAO.req.body.code : TAO.req.query.code) {
-      case 'version' :
+      case 'PipVersion' :
           _f['python'] = function(cbk) {
                exec('pip --version', {maxBuffer: 1024 * 20480},
                     function(error, stdout, stderr) {
@@ -15,7 +15,34 @@ switch((TAO.req.body.code) ? TAO.req.body.code : TAO.req.query.code) {
                });
           }
           _f['python3'] = function(cbk) {
-               exec('pip3 --version', {maxBuffer: 1024 * 20480},
+               exec('pip3n3 --version', {maxBuffer: 1024 * 20480},
+                    function(error, stdout, stderr) {
+                       if (error) {
+                        cbk(error.message.replace(/\n/ig, '++'));
+                       } else {
+                         cbk(JSON.parse(stdout));
+                       }	
+               });
+          }
+          CP.serial(
+               _f,
+               function(data) {
+                    TAO.res.send({python : CP.data.python, python3 : CP.data.python3});
+               }, 6000);   
+          break;
+      case 'PythonVersion' :
+          _f['python'] = function(cbk) {
+               exec('python --version', {maxBuffer: 1024 * 20480},
+                    function(error, stdout, stderr) {
+                       if (error) {
+                        cbk(error.message.replace(/\n/ig, '++'));
+                       } else {
+                         cbk(JSON.parse(stdout));
+                       }	
+               });
+          }
+          _f['python3'] = function(cbk) {
+               exec('python3 --version', {maxBuffer: 1024 * 20480},
                     function(error, stdout, stderr) {
                        if (error) {
                         cbk(error.message.replace(/\n/ig, '++'));
