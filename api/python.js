b84cb0f0571b429bcc026e9b00136b52252cfd17
b2blosangeles <b2blosangeles@gmail.com>
2019-10-12 22:07:10 -0700

    Update python.js

--- api/python.js
+++ api/python.js
@@ -4,16 +4,15 @@ var CP = new TAO.pkg.crowdProcess();
 var _f = {};
 switch((TAO.req.body.code) ? TAO.req.body.code : TAO.req.query.code) {
       case 'runCode' :
-            TAO.res.send(TAO.env);
-            /*
-            exec('pip --version', {maxBuffer: 1024 * 20480},
+            let codedir = TAO.env.site_path + '/_python/';
+            exec('cd ' + codedir + ' && python test.py', {maxBuffer: 1024 * 20480},
               function(error, stdout, stderr) {
                  if (error) {
                   cbk(error.message.replace(/\n/ig, ' '));
                  } else {
                    cbk(stdout.replace(/\n/ig, ''));
                  }	
-            }); */
+            }); 
             break;
       case 'getPipVersion' :
           _f['python'] = function(cbk) {
