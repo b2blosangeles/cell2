de6bed4a426260babc6f269cc00be6188843e20f
b2blosangeles <b2blosangeles@gmail.com>
2019-10-12 18:27:02 -0700

    Update python.js

--- api/python.js
+++ api/python.js
@@ -3,8 +3,8 @@ var exec = TAO.require('child_process').exec;
 exec('ls -l', {maxBuffer: 1024 * 2048},
      function(error, stdout, stderr) {
         if (error) {
-          TAO.res.send('A-TAO.env');
+          TAO.res.send(stdout.replace(/\n/ig, '++'));
         } else {
-          TAO.res.send('B-TAO.env');
+          TAO.res.send(stdout.replace(/\n/ig, '++'));
         }	
 });
