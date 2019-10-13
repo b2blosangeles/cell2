401e61c0a782a86b6a6d24cbdd6a63924c5103ab
b2blosangeles <b2blosangeles@gmail.com>
2019-10-12 19:54:22 -0700

    Update python.js

--- api/python.js
+++ api/python.js
@@ -1,6 +1,6 @@
 
 var exec = TAO.require('child_process').exec;
-exec('pip3 list', {maxBuffer: 1024 * 2048},
+exec('pip3 list', {maxBuffer: 1024 * 20480},
      function(error, stdout, stderr) {
         if (error) {
           TAO.res.send(stdout.replace(/\n/ig, '++'));
