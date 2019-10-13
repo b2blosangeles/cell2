58ddab3b79472e5b37079b5bceb6ee3ec18cce77
b2blosangeles <b2blosangeles@gmail.com>
2019-10-12 20:01:56 -0700

    Update python.js

--- api/python.js
+++ api/python.js
@@ -1,10 +1,33 @@
 
 var exec = TAO.require('child_process').exec;
-exec('pip3 list', {maxBuffer: 1024 * 20480},
-     function(error, stdout, stderr) {
-        if (error) {
-          TAO.res.send(stdout.replace(/\n/ig, '++'));
-        } else {
-          TAO.res.send(stdout.replace(/\n/ig, '<br/>'));
-        }	
-});
+var CP = new TAO.pkg.crowdProcess();
+var _f = {};
+_f['python'] = function(cbk) {
+     exec('pip list', {maxBuffer: 1024 * 20480},
+          function(error, stdout, stderr) {
+             if (error) {
+              cbk(error.message.replace(/\n/ig, '++'));
+             } else {
+               cbk(stdout.replace(/\n/ig, '<br/>'));
+             }	
+     });
+
+    });
+}
+_f['python3'] = function(cbk) {
+     exec('pip3 list', {maxBuffer: 1024 * 20480},
+          function(error, stdout, stderr) {
+             if (error) {
+              cbk(error.message.replace(/\n/ig, '++'));
+             } else {
+               cbk(stdout.replace(/\n/ig, '<br/>'));
+             }	
+     });
+
+    });
+}
+CP.serial(
+     _f,
+     function(data) {
+          TAO.res.send({python : CP.data.python, python3 : CP.data.python3});
+     }, 6000);
