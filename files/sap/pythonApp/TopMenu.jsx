45f86db0dca439c8bc66cc46181dd4ba65bb2838
b2blosangeles <b2blosangeles@gmail.com>
2019-10-13 09:12:09 -0700

    update

--- files/sap/pythonApp/TopMenu.jsx
+++ files/sap/pythonApp/TopMenu.jsx
@@ -11,21 +11,6 @@ class TopMenu extends React.Component {
   }
   componentDidMount() {
 	var me = this;
-
-	ReactDOM.TAO.dataEngine({
-	     type: 'POST',
-	     url: '/api/Tao/API_PG.api?code=PGdatabases',
-	     data: {},
-	     dataType: 'JSON',
-	     timeout: (6 * 1000),
-	     success: function(resultData){
-		  me.setState({list : resultData.data, _TM : new Date().getTime()});
-	     },
-	     error : function(err) { 
-		     console.log('err');
-	     }, 
-	     spinner : me	
-	});
   }
   componentDidUpdate(prevProps, prevState) {
 	var me = this;
