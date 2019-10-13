0462c983983fbbd109d400afebe52e6fd0ff4025
b2blosangeles <b2blosangeles@gmail.com>
2019-10-13 09:08:44 -0700

    update

--- files/sap/pythonApp/TopMenu.jsx
+++ files/sap/pythonApp/TopMenu.jsx
@@ -4,7 +4,7 @@ class TopMenu extends React.Component {
 	this.props = props;
 	this.state = {list : []};
 	this.menu = [
-		{caption : 'Packages', code : 'getPackages'},
+		{caption : 'Pip packages', code : 'getPackages'},
 		{caption : 'Add a package', code : 'addPackage'},
 		{caption : 'Run a python code', code : 'runCode'}
 	]
@@ -37,7 +37,7 @@ class TopMenu extends React.Component {
 	var me = this;
 	return (
 	<div className="border border-success alert-success rounded  m-0 mt-3 mb-2 p-2">
-		<h1>Python Integration</h1>
+		<h1>Python Integration Tool (V1.0)</h1>
 		<p className="p-2">
 			{this.menu.map(function(item, i){
 				return (<span className="mr-3">
