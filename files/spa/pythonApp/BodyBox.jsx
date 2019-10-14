559801de4d612cf5a3d538b9e24c1b947b1e7fa8
b2blosangeles <b2blosangeles@gmail.com>
2019-10-13 18:18:34 -0700

    update

--- files/spa/pythonApp/BodyBox.jsx
+++ files/spa/pythonApp/BodyBox.jsx
@@ -6,7 +6,7 @@ class BodyBox extends React.Component {
   }
   componentDidMount() {
 	var me = this; 
-	
+	alert(me.props.code)
 	ReactDOM.TAO.dataEngine({
 	     type: 'POST',
 	     url: '/api/Tao/API_PG.api?code=PGtables',
@@ -30,7 +30,7 @@ class BodyBox extends React.Component {
     var me = this;
     return (
       <div className="border border-warning alert-warning rounded m-0 p-2">
-	ALL TABLES :
+	ALL TABLES : {me.props.code}
         <ul>
           {this.state.list.map(function(item, i){
              return (<li>{item.tablename}</li>)
