aee1cd52ecdeb7c39f11eb8b72f4d94c73217600
b2blosangeles <b2blosangeles@gmail.com>
2019-10-13 21:01:13 -0700

    update

--- files/spa/pythonApp/BodyBox.jsx
+++ files/spa/pythonApp/BodyBox.jsx
@@ -8,21 +8,27 @@ class BodyBox extends React.Component {
 	var me = this; 
   }
   loadData(code) {
-	var me = this; 
-	ReactDOM.TAO.dataEngine({
-		type: 'POST',
-		url: '/api/Tao/API_PG.api?code=PGtables',
-		data: {},
-		dataType: 'JSON',
-		timeout: (6 * 1000),
-		success: function(resultData){
-		me.setState({list : resultData.data, _TM : new Date().getTime()});
-		},
-		error : function(err) { 
-			console.log('err');
-		}, 
-		spinner : me
-	});   
+	var me = this;
+	switch (code) {
+		case 'getPackages' :
+			ReactDOM.TAO.dataEngine({
+				type: 'POST',
+				url: '/api/Tao/API_PG.api?code=PGtables',
+				data: {},
+				dataType: 'JSON',
+				timeout: (6 * 1000),
+				success: function(resultData){
+				me.setState({list : resultData.data, _TM : new Date().getTime()});
+				},
+				error : function(err) { 
+					console.log('err');
+				}, 
+				spinner : me
+			});
+			break;
+		default :
+	}
+  
   }
   componentDidUpdate(prevProps, prevState) {
 	var me = this;
@@ -34,25 +40,27 @@ class BodyBox extends React.Component {
 		}
 	}
   }
-
   render() {
 	var me = this;
-	if (me.state.code === 'getPackages') {
-		return (
-			<div className="border border-warning alert-warning rounded m-0 p-2">
-			  {me.state.caption}
-			  <ul>
-				{this.state.list.map(function(item, i){
-				   return (<li>{item.tablename}</li>)
-				 })}
-			  </ul>
-			</div>
-		  );
-	} else {
-		return (<div className="border border-warning alert-warning rounded m-0 p-2">
-			  {me.state.caption}--
-			  </div>
-		  );
+	switch (me.state.code) {
+		case 'getPackages' :
+			return (
+				<div className="border border-warning alert-warning rounded m-0 p-2">
+				  {me.state.caption}
+				  <ul>
+					{this.state.list.map(function(item, i){
+					   return (<li>{item.tablename}</li>)
+					 })}
+				  </ul>
+				</div>
+			  );
+			break;
+		default :
+			return (<div className="border border-warning alert-warning rounded m-0 p-2">
+				{me.state.caption}-A-
+				</div>
+			);
+			break;
 	}
   }
 }
