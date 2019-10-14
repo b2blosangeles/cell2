f54845c7fb03403a9e1d5af62843ce03bbd27edb
b2blosangeles <b2blosangeles@gmail.com>
2019-10-13 19:47:26 -0700

    update

--- files/spa/pythonApp/TaoController.jsx
+++ files/spa/pythonApp/TaoController.jsx
@@ -5,7 +5,6 @@
         load : function(id, obj, pobj) {
 			obj.props._TAOID = id;
 			delete this.list[id];
-			console.log('--niu--');
 			if (!this.list.Root && id !== 'Root') {
 				this.loadRoot();
 			}
@@ -23,21 +22,21 @@
 
 		append  : function(id, obj, pobj) {
 			var newObj = pobj.appendChild( document.createElement( 'div' ));
-				this.load(id, obj, newObj);
-			},
-			setState : function(id, data) {
-				if (id === '*') {
-					for (o in this.list) {
-						this.list[o].setState({_TAOTM: new Date().getTime(), _TAOstate: data});
-					}
-				} else if (Array.isArray(id)) {
-					for (var i = 0; i < id.length; i++) {
-						if (typeof this.list[id[i]] === 'object') {
-							this.list[id[i]].setState({_TAOTM: new Date().getTime(), _TAOstate: data});
-						}
+			this.load(id, obj, newObj);
+		},
+		setState : function(id, data) {
+			if (id === '*') {
+				for (o in this.list) {
+					this.list[o].setState({_TAOTM: new Date().getTime(), _TAOstate: data});
+				}
+			} else if (Array.isArray(id)) {
+				for (var i = 0; i < id.length; i++) {
+					if (typeof this.list[id[i]] === 'object') {
+						this.list[id[i]].setState({_TAOTM: new Date().getTime(), _TAOstate: data});
 					}
 				}
-			},
+			}
+		},
 		loadRoot : function() {
 			this.append('Root', <TAORoot param={{}} />, document.body);
 		},
