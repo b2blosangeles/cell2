fbf96a6fee23475501660ac0440d215c77d31fc5
b2blosangeles <b2blosangeles@gmail.com>
2019-10-14 13:11:04 -0700

    Update BodyBox.jsx

--- files/spa/pythonApp/BodyBox.jsx
+++ files/spa/pythonApp/BodyBox.jsx
@@ -167,7 +167,7 @@ class BodyBox extends React.Component {
 								<div className="container-fluid">
 									<div className="row">
 										<div className="col-sm-10 p-0 pl-2 pr-2">
-											{me.state.pythonCodeResult}
+											{JSON.stringify(me.state.pythonCodeResult)}
 										</div>
 									</div>
 								</div>
