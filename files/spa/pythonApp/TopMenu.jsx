4b386df25918d33fb3cb6cfcc6f314200420327e
b2blosangeles <b2blosangeles@gmail.com>
2019-10-14 23:50:44 -0700

    Update TopMenu.jsx

--- files/spa/pythonApp/TopMenu.jsx
+++ files/spa/pythonApp/TopMenu.jsx
@@ -34,7 +34,7 @@ class TopMenu extends React.Component {
 						{item.caption}
 						</a> 
 					</span>) : (<span className="mr-3">
-						<button className="border border-secondary bg-secondary rounded p-2">{item.caption}</button> 
+						<a className="border border-secondary bg-secondary rounded p-2">{item.caption}</a> 
 					</span>)
 			})}
 		</p>
