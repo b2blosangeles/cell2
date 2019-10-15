f7c4a61ad6c34f74bd5ec507eaee9e545a59b852
b2blosangeles <b2blosangeles@gmail.com>
2019-10-14 23:25:48 -0700

    Update RunCode.jsx

--- files/spa/pythonApp/RunCode.jsx
+++ files/spa/pythonApp/RunCode.jsx
@@ -40,39 +40,39 @@ class RunCode extends React.Component {
     render() {
         var me = this;
         return(<div className="border border-warning alert-warning rounded m-0 p-2 bodyBox">
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col-sm-4 p-0">
-                    <h5>{me.state.caption}</h5>
-                </div>
-                <div className="col-sm-4 p-0">
-                {(me.state.pythonType === 'python') ? (<b>Python</b>) : 
-                (<a href="javascript:void(0);" onClick={me.switchPythonType.bind(me, 'python')}>Python</a>)
-                }</div>
-                <div className="col-sm-4 p-0">
-                {(me.state.pythonType === 'python3') ? (<b>Python3</b>) : 
-                (<a href="javascript:void(0);" onClick={me.switchPythonType.bind(me, 'python3')}>Python3</a>)
-                }</div>
-            </div>
-        </div>
-        <hr/>
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col-sm-2 p-0 pl-2 pr-2">
-                {(!this.state.pythonCodes) ? '' :
-                (<ul>
-                    {this.state.pythonCodes.map(function(item, i){
-                    return (<li><a href="javascript:void(0);" onClick={me.runCode.bind(me, item)}>{item}</a></li>)
-                    })}
-                </ul>)
-                }
-                </div>
-                <div className="col-sm-10 p-0 m-0">
-                    <CodeResult codeFile={me.state.codeFile} pythonType={me.state.pythonType} parent={me} />
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-sm-4 p-0">
+                        <h5>{me.state.caption}</h5>
+                    </div>
+                    <div className="col-sm-4 p-0">
+                    {(me.state.pythonType === 'python') ? (<b>Python</b>) : 
+                    (<a href="javascript:void(0);" onClick={me.switchPythonType.bind(me, 'python')}>Python</a>)
+                    }</div>
+                    <div className="col-sm-4 p-0">
+                    {(me.state.pythonType === 'python3') ? (<b>Python3</b>) : 
+                    (<a href="javascript:void(0);" onClick={me.switchPythonType.bind(me, 'python3')}>Python3</a>)
+                    }</div>
                 </div>
             </div>
+            <hr/>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-sm-2 p-0 pl-2 pr-2">
+                    {(!this.state.pythonCodes) ? '' :
+                    (<ul>
+                        {this.state.pythonCodes.map(function(item, i){
+                        return (<li><a href="javascript:void(0);" onClick={me.runCode.bind(me, item)}>{item}</a></li>)
+                        })}
+                    </ul>)
+                    }
+                    </div>
+                    <div className="col-sm-10 p-0 m-0">
+                        <CodeResult codeFile={me.state.codeFile} pythonType={me.state.pythonType} parent={me} />
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-  );)
+        );
     }
   }
