f5117e292178e84b23e44b857b8fd92482d6355b
b2blosangeles <b2blosangeles@gmail.com>
2019-10-14 23:33:05 -0700

    update

--- files/spa/pythonApp/BodyBox.jsx
+++ files/spa/pythonApp/BodyBox.jsx
@@ -2,7 +2,7 @@ class BodyBox extends React.Component {
   constructor(props) {
 	super(props);
 	this.props = props;
-	this.state = {pythonType : 'python'};
+	this.state = {};
   }
   componentDidMount() {
 	var me = this;
@@ -71,18 +71,6 @@ class BodyBox extends React.Component {
 	}
   
   }
-  runCode (codefn) {
-	var me = this;
-	// me.setState({codeFile : null}, function() {
-	//	console.log('--00700--');
-		me.setState({codeFile : codefn});
-	//});
-
-  }
-  switchPythonType(type) {
-	var me = this;
-	me.setState({pythonType : type});
-  }
   componentDidUpdate(prevProps, prevState) {
 	var me = this;
 	if (me.state._TAOTM !== prevState._TAOTM) {
@@ -131,45 +119,6 @@ class BodyBox extends React.Component {
 		case 'getCodes' :
 			return(<RunCode caption={me.state.caption} />);
 			break;
-			/*
-					return (
-						<div className="border border-warning alert-warning rounded m-0 p-2 bodyBox">
-							<div className="container-fluid">
-								<div className="row">
-									<div className="col-sm-4 p-0">
-										<h5>{me.state.caption}</h5>
-									</div>
-									<div className="col-sm-4 p-0">
-									{(me.state.pythonType === 'python') ? (<b>Python</b>) : 
-									(<a href="javascript:void(0);" onClick={me.switchPythonType.bind(me, 'python')}>Python</a>)
-									}</div>
-									<div className="col-sm-4 p-0">
-									{(me.state.pythonType === 'python3') ? (<b>Python3</b>) : 
-									(<a href="javascript:void(0);" onClick={me.switchPythonType.bind(me, 'python3')}>Python3</a>)
-									}</div>
-								</div>
-							</div>
-							<hr/>
-							<div className="container-fluid">
-								<div className="row">
-									<div className="col-sm-2 p-0 pl-2 pr-2">
-									{(!this.state.pythonCodes) ? '' :
-									(<ul>
-										{this.state.pythonCodes.map(function(item, i){
-										return (<li><a href="javascript:void(0);" onClick={me.runCode.bind(me, item)}>{item}</a></li>)
-										})}
-									</ul>)
-									}
-									</div>
-									<div className="col-sm-10 p-0 m-0">
-										<CodeResult codeFile={me.state.codeFile} pythonType={me.state.pythonType} parent={me} />
-									</div>
-								</div>
-							</div>
-						</div>
-					  );
-					break;
-					*/
 		default :
 			return (<div className="border border-warning alert-warning rounded m-0 p-3 bodyBox">
 					<div className="container mt-3">
