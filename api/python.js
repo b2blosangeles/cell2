2cf123f0b0ebec545fe93b75670fa70963e6edc5
b2blosangeles <b2blosangeles@gmail.com>
2019-10-14 21:17:12 -0700

    Update python.js

--- api/python.js
+++ api/python.js
@@ -35,7 +35,7 @@ switch((TAO.req.body.code) ? TAO.req.body.code : TAO.req.query.code) {
                       setTimeout(
                          function() {
                               TAO.res.send({
-                                   pythonType : ythonType,
+                                   pythonType : pythonType,
                                    data: stdout.replace(/(\n|\r|\t)/gi, '')});
                          }, 100
 
