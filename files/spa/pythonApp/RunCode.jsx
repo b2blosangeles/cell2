13eb418b71a647e63ad7e106b1a9a5db22ac3eb6
b2blosangeles <b2blosangeles@gmail.com>
2019-10-15 00:33:30 -0700

    update

--- files/spa/pythonApp/RunCode.jsx
+++ files/spa/pythonApp/RunCode.jsx
@@ -95,7 +95,7 @@ class RunCode extends React.Component {
                                 <div className="container">
                                     <div className="row">
                                         <div className="col-sm-12 p-0">
-                                            {me.props.codeFile }
+                                            {me.state.codeFile }
                                         </div>
                                     </div>
                                     <div className="row">
