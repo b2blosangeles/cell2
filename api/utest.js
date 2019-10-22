ac4e8eebe3a3d2cb15fd6fd65a7a1010bbfeb4a8
b2blosangeles <b2blosangeles@gmail.com>
2019-10-22 09:28:37 -0700

    Update utest.js

--- api/utest.js
+++ api/utest.js
@@ -1,6 +1,6 @@
 delete TAO.require.cache[TAO.env.site_path + '/api/shell.inc.js'];
 var SHELL = TAO.require(TAO.env.site_path + '/api/shell.inc.js');
 var shell = new SHELL(TAO);
-shell.exec('python3 -tV', function(data){
+shell.exec('python3 -tV234', function(data){
     TAO.res.send(data);
 });
