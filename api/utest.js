a44a2f5be3932f97d4b40a7f496e56f490ef853b
b2blosangeles <b2blosangeles@gmail.com>
2019-10-22 14:00:59 -0700

    Update utest.js

--- api/utest.js
+++ api/utest.js
@@ -1,4 +1,4 @@
 var shell = new TAO.pkg.commandShell();
-shell.batchExec('cd /var && pwd && cd /var/tao && pwd && python -V && python3 -V', function(data){
+shell.batchExec('cd \/var && pwd && cd \/var\/tao && pwd && python -V && python3 -V', function(data){
     TAO.res.send(data);
 });
