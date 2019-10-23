cc3102fc4d2d23451c7286787413e7c46144cb5f
b2blosangeles <b2blosangeles@gmail.com>
2019-10-23 16:34:24 -0700

    Create PythonVersion.js

---
+++ files/spa/pythonApp/PythonVersion.js
@@ -0,0 +1,59 @@
+class PythonVersion extends React.Component {
+    constructor(props) {
+      super(props);
+      this.props = props;
+      this.state = {};
+    }
+    componentDidMount() {
+      var me = this;
+      me.setState({pythonPackegs: null});
+      ReactDOM.TAO.dataEngine({
+          type: 'POST',
+          url: '/api/python.api',
+          data: {code : 'getPackages'},
+          dataType: 'JSON',
+          timeout: (6 * 1000),
+          success: function(resultData){
+              me.setState({pythonPackegs: resultData});
+              
+          },
+          error : function(err) { 
+              console.log('err');
+          }, 
+          spinner : me
+      });
+    }
+    componentDidUpdate(prevProps, prevState) {
+        var me = this;
+    }
+    render() {
+        var me = this;
+        return(<div className="border border-secondary bg-light rounded m-0 p-2 bodyBox">
+            <h5>{this.props.caption}</h5>
+            <hr/>
+              <div className="container-fluid">
+                  <div className="row">
+                      {(!this.state.pythonPackegs || !this.state.pythonPackegs.python) ? '' :
+                          (<div className="col-sm-6 p-0 pl-2 pr-2">
+                          Python packages:
+                          <ul>
+                              {this.state.pythonPackegs.python.map(function(item, i){
+                              return (<li>{item.name} ({item.version})</li>)
+                              })}
+                          </ul>
+                      </div>)}
+                      {(!this.state.pythonPackegs || !this.state.pythonPackegs.python3) ? '' :
+                          (<div className="col-sm-6 p-0 pl-2 pr-2">
+                          Python3 packages:
+                          <ul>
+                              {this.state.pythonPackegs.python3.map(function(item, i){
+                              return (<li>{item.name} ({item.version})</li>)
+                              })}
+                          </ul>
+                      </div>)}
+                  </div>
+              {ReactDOM.TAO.list.Root.showSpinner(me)}
+              </div>
+          </div>);
+    }
+  }
