b70eb27b5ddac4c8294bd4919cf5dcad302a707d
b2blosangeles <b2blosangeles@gmail.com>
2019-10-22 18:48:42 -0700

    Update python.js

--- api/python.js
+++ api/python.js
@@ -46,68 +46,17 @@ switch((TAO.req.body.code) ? TAO.req.body.code : TAO.req.query.code) {
             }); 
 
             break;
-      case 'getPipVersion' :
-          _f['python'] = function(cbk) {
-               exec('pip --version', {maxBuffer: 1024 * 20480},
-                    function(error, stdout, stderr) {
-                       if (error) {
-                        cbk(error.message.replace(/(\n|\r|\t)/gi, ' '));
-                       } else {
-                         cbk(stdout.replace(/(\n|\r|\t)/gi, ''));
-                       }	
-               });
-          }
-          _f['python3'] = function(cbk) {
-               exec('pip3 --version', {maxBuffer: 1024 * 20480},
-                    function(error, stdout, stderr) {
-                       if (error) {
-                        cbk(error.message.replace(/(\n|\r|\t)/gi, ' '));
-                       } else {
-                         cbk(stdout.replace(/(\n|\r|\t)/gi, ''));
-                       }	
-               });
-          }
-          CP.serial(
-               _f,
-               function(data) {
-                    let ret = {};
-                    ret.python = (CP.data.python) ? CP.data.python : null;
-                    ret.python3 = (CP.data.python3) ? CP.data.python3 : null;
-                    TAO.res.send(ret);
-               }, 6000);   
+            
+      case 'getPythonVersion' : 
+          var shell = new TAO.pkg.commandShell();
+          shell.run('pip --version && pip3 --version', function(data){
+                let ret = {};
+                ret.python = (data.results.P_0.status === 'success') ? data.results.P_0.data : '';
+                ret.python3 = (data.results.P_1.status === 'success') ? data.results.P_1.data : '';
+                TAO.res.send(ret);
+            });
           break;
-      case 'getPythonVersion' :
-          _f['python'] = function(cbk) {
-
-               exec('python -V', {maxBuffer: 1024 * 20480},
-                    function(error, stdout, stderr) {
-                         if (error) {
-                              cbk(error.message.replace(/(\n|\r|\t)/gi, ' '));
-                         } else {
-                              cbk(stdout.replace(/(\n|\r|\t)/gi, ''));
-                         }	
-               });
-          }
-          /*
-          _f['python3'] = function(cbk) {
-               exec('python3 --version && python --version', {maxBuffer: 1024 * 20480},
-                    function(error, stdout, stderr) {
-                       if (error) {
-                        cbk(error.message.replace(/(\n|\r|\t)/gi, ' '));
-                       } else {
-                         cbk(stdout.replace(/(\n|\r|\t)/gi, ''));
-                       }	
-               });
-          }*/
-          CP.serial(
-               _f,
-               function(data) {
-                    let ret = {};
-                    ret.python = (CP.data.python) ? CP.data.python : null;
-                    ret.python3 = (CP.data.python3) ? CP.data.python3 : null;
-                    TAO.res.send(data);
-               }, 6000);   
-          break;
+            
       case 'getPipVersion' : 
           var shell = new TAO.pkg.commandShell();
           shell.run('pip --version && pip3 --version', function(data){
@@ -117,6 +66,7 @@ switch((TAO.req.body.code) ? TAO.req.body.code : TAO.req.query.code) {
                 TAO.res.send(ret);
             });
           break;
+            
       case 'getPackages' : 
           var shell = new TAO.pkg.commandShell();
           shell.run('pip list  --format=json && pip3 list  --format=json', function(data){
