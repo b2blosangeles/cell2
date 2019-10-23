aed138500c5017592961c578deda22ee298a814b
b2blosangeles <b2blosangeles@gmail.com>
2019-10-23 08:36:19 -0700

    Update RunCode.jsx

--- files/spa/pythonApp/RunCode.jsx
+++ files/spa/pythonApp/RunCode.jsx
@@ -85,7 +85,7 @@ class RunCode extends React.Component {
                     <div className="col-sm-8 p-0 m-0">
                     
                         <div className="m-0 p-0 bodyBox">
-                                <div className="container">
+                                <div className="container-fluid p-2 border border-secondary rounded">
                                     <div className="row">
                                         <div className="col-sm-12 p-2 text-right">
                                             <div className="btn-group border border-secondary rounded">
