a1bc27bf5bd257613e86373251fc0892e0515beb
b2blosangeles <b2blosangeles@gmail.com>
2019-10-23 15:33:26 -0700

    Update RunCode.jsx

--- files/spa/pythonApp/RunCode.jsx
+++ files/spa/pythonApp/RunCode.jsx
@@ -86,8 +86,8 @@ class RunCode extends React.Component {
                     <div className="col-sm-9 p-0 border border-secondary rounded shadow-sm">
                         <div className="container ">
                             <div className="row">
-                                <div className="col-sm-12 p-2 bg-light text-right rounded">
-                                    <div className="btn-group border border-secondary rounded">
+                                <div className="col-sm-12 p-2 alert-secondary text-right rounded">
+                                    <div className="btn-group border border-dark rounded">
                                           {(me.state.pythonType === 'python') ? (<button className="btn  disabled rounded-left">
                                           <i className="fa fa-check-square-o mr-2" aria-hidden="true"></i>
                                           Python</button>)
