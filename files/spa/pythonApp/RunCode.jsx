02c8e9a1c59cfc59475c198fd8dedeb1b0217138
b2blosangeles <b2blosangeles@gmail.com>
2019-10-22 23:50:36 -0700

    Update RunCode.jsx

--- files/spa/pythonApp/RunCode.jsx
+++ files/spa/pythonApp/RunCode.jsx
@@ -64,7 +64,7 @@ class RunCode extends React.Component {
         return(<div className="border border-warning alert-warning rounded m-0 p-2 bodyBox">
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-sm-4 p-2 m-2 border-secondary">
+                    <div className="col-sm-4 p-2 border-secondary rounded">
                         <h5>{this.props.caption}</h5>
                         <br/>
                         <div className="container-fluid">
