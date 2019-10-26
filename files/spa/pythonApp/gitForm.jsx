0196947c3a585124fb1527e98b65bd6eb934af96
b2blosangeles <b2blosangeles@gmail.com>
2019-10-25 22:19:23 -0700

    Update gitForm.jsx

--- files/spa/pythonApp/gitForm.jsx
+++ files/spa/pythonApp/gitForm.jsx
@@ -95,7 +95,7 @@ class GitForm extends React.Component {
   	 var me = this;
 	 var isEnabled = me.isSubmitEnabled();
 	 return (<div className="border border-secondary alert-secondary rounded shadow p-2 rightBox-containner">
-			<h3 className="p-3">Add Virtual Space</h3>	    
+			<h3 className="p-3">Parking</h3>	    
 			<hr/>			    
 			<form>
 				<div className="container container-fluid">
